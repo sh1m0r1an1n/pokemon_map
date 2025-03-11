edf6d64fc4f98010ed4bc07ae8150877d15370b5
sh1m0r1an1n <akhmanaev2021@gmail.com>
2025-03-11 19:19:02 +0500

    show pokemons on mainpage

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -2,6 +2,7 @@ import folium
 import json
 
 from django.http import HttpResponseNotFound
+from .models import Pokemon, PokemonEntity
 from django.shortcuts import render
 
 
@@ -39,12 +40,18 @@ def show_all_pokemons(request):
                 pokemon['img_url']
             )
 
+    pokemons = Pokemon.objects.prefetch_related('pokemonentity_set').all()
     pokemons_on_page = []
     for pokemon in pokemons:
+        if pokemon.photo:
+            img_url = request.build_absolute_uri(pokemon.photo.url)
+        else:
+            img_url = DEFAULT_IMAGE_URL
+
         pokemons_on_page.append({
-            'pokemon_id': pokemon['pokemon_id'],
-            'img_url': pokemon['img_url'],
-            'title_ru': pokemon['title_ru'],
+            'pokemon_id': pokemon.id,
+            'img_url': img_url,
+            'title_ru': pokemon.title,
         })
 
     return render(request, 'mainpage.html', context={
