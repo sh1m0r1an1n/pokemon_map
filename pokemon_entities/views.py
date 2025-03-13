046262b363702dafde311efae6e3a29a02e3e090
sh1m0r1an1n <akhmanaev2021@gmail.com>
2025-03-13 21:00:03 +0500

    Добавил шорткат get_object_or_404

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,7 +1,6 @@
 import folium
 
-from django.http import HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Pokemon, PokemonEntity
 
@@ -71,10 +70,7 @@ def show_all_pokemons(request):
 
 
 def show_pokemon(request, pokemon_id):
-    try:
-        pokemon = Pokemon.objects.get(id=pokemon_id)
-    except Pokemon.DoesNotExist:
-        return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
+    pokemon = get_object_or_404(Pokemon, id=pokemon_id)
 
     time_now = timezone.localtime()
 
